f1131e29c5bf3b3a474afa3d6ad71b364e470992
Muhammad Ali Dzulfiqar <malidzulfiqar@gmail.com>
2022-03-28 14:49:14 -0700

    modified ls

--- cd_ls_pwd.c
+++ cd_ls_pwd.c
@@ -25,14 +25,53 @@ int cd()
 
 int ls_file(MINODE *mip, char *name)
 {
-  printf("ls_file: to be done: READ textbook!!!!\n");
-  // READ Chapter 11.7.3 HOW TO ls
+  char *t1 = "xwrxwrxwr-------";
+  char *t2 = "----------------";
+
+  char ftime[256];
+  u16 mode = mip->INODE.i_mode;
+
+  if (S_ISREG(mode)) 
+      printf("%c", '-');
+  if (S_ISDIR(mode))
+      printf("%c", 'd');
+  if (S_ISLNK(mode))
+      printf("%c", 'l');
+  for (int i = 8; i >= 0; i--)
+  {
+      if (mode & (1 << i))
+          printf("%c", t1[i]); // print r|w|x printf("%c", t1[i]);
+      else
+          printf("%c", t2[i]); // or print -
+  }
+  printf("%4d ", mip->INODE.i_links_count); // link count
+  printf("%4d ", mip->INODE.i_gid);         // gid
+  printf("%4d ", mip->INODE.i_uid);         // uid
+  printf("%8d ", mip->INODE.i_size);       // file size
+
+  strcpy(ftime, ctime((time_t *)&(mip->INODE.i_mtime))); // print time in calendar form ftime[strlen(ftime)-1] = 0; // kill \n at end
+  ftime[strlen(ftime) - 1] = 0;                // removes the \n
+  printf("%s ", ftime);                        // prints the time
+
+  printf("%s", name);
+  if (S_ISLNK(mode))
+  {
+    printf(" -> %s", (char *)mip->INODE.i_block); // print linked name 
+  }
+
+  printf("\n");
   return 0;
 }
 
 int ls_dir(MINODE *mip)
 {
-  printf("ls_dir: list CWD's file names; YOU FINISH IT as ls -l\n");
+  char *t1 = "xwrxwrxwr-------";
+  char *t2 = "----------------";
+
+  char ftime[256];
+  MINODE *temp_mip;
+
+  u16 mode = mip->INODE.i_mode;
 
   char buf[BLKSIZE], temp[256];
   DIR *dp;
@@ -46,7 +85,10 @@ int ls_dir(MINODE *mip)
      strncpy(temp, dp->name, dp->name_len);
      temp[dp->name_len] = 0;
 	
-     printf("%s  ", temp);
+     temp_mip = iget(dev, dp->inode);
+     ls_file(temp_mip, temp);
+     temp_mip->dirty = 1;
+     iput(temp_mip);
 
      cp += dp->rec_len;
      dp = (DIR *)cp;
@@ -56,10 +98,34 @@ int ls_dir(MINODE *mip)
   return 0;
 }
 
-int ls()
+int ls(char *pathname)
 {
-  printf("ls: list CWD only! YOU FINISH IT for ls pathname\n");
-  ls_dir(running->cwd);
+  int mode;
+  if(strcmp(pathname, ""))
+  {
+    ls_dir(running->cwd);
+  }
+  else{
+    int ino = getino(pathname);
+    if(ino == -1)
+    {
+      printf("INODE DO NOT EXIST\n");
+      return -1;
+    }
+    else{
+      dev = root->dev;
+      MINODE *mip = iget(dev, ino);
+      mode = mip->INODE.i_mode;
+      if (S_ISDIR(mode))
+      {
+        ls_dir(mip);
+      }
+      else
+      {
+        ls_file(mip, basename(pathname));
+      }
+    }
+  }
   return 0;
 }
 
