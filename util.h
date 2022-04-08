b1f2bc3590ceade0a79ca41cedc7dada16511640
Jing Ren Tay <jingren.tay@wsu.edu>
2022-04-08 00:58:04 -0700

    Added util.h

---
+++ util.h
@@ -0,0 +1,20 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include "type.h"
+
+int init();
+int mount_root();
+int quit();
+
+int get_block(int dev, int blk, char *buf);
+int put_block(int dev, int blk, char *buf);
+int tokenize(char *pathname);
+MINODE *iget(int dev, int ino);
+void iput(MINODE *mip);
+int search(MINODE *mip, char *name);
+int getino(char *pathname);
+int findmyname(MINODE *parent, u32 myino, char myname[ ]);
+int findino(MINODE *mip, u32 *myino);
+
+#endif
